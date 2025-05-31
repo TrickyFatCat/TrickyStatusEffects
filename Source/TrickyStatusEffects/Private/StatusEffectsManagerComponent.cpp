99bb3b505b054b1cd6aac420c45b3cf87f835a6d
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-05-31 19:36:40 +0300

    Create status effect manager component

---
+++ Source/TrickyStatusEffects/Private/StatusEffectsManagerComponent.cpp
@@ -0,0 +1,10 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "StatusEffectsManagerComponent.h"
+
+
+UStatusEffectsManagerComponent::UStatusEffectsManagerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
---
+++ Source/TrickyStatusEffects/Public/StatusEffectsManagerComponent.h
@@ -0,0 +1,17 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "StatusEffectsManagerComponent.generated.h"
+
+
+UCLASS(ClassGroup=(TrickyStatusEffects), meta=(BlueprintSpawnableComponent))
+class TRICKYSTATUSEFFECTS_API UStatusEffectsManagerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UStatusEffectsManagerComponent();
+};
