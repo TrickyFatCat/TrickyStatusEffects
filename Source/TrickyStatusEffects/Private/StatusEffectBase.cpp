648500951fe6556ea5de0a6d3cbe1f577df26804
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-05-31 19:37:00 +0300

    Create base status effect object

---
+++ Source/TrickyStatusEffects/Private/StatusEffectBase.cpp
@@ -0,0 +1,96 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "StatusEffectBase.h"
+
+#include "StatusEffectsManagerComponent.h"
+
+void UStatusEffectBase::Tick(float DeltaTime)
+{
+	TickEvent(DeltaTime);
+}
+
+bool UStatusEffectBase::IsTickable() const
+{
+	return !IsUnreachable();
+}
+
+bool UStatusEffectBase::IsTickableWhenPaused() const
+{
+	return !IsUnreachable();
+}
+
+UWorld* UStatusEffectBase::GetTickableGameObjectWorld() const
+{
+	if (!IsValid(GetOuter()))
+	{
+		return nullptr;
+	}
+
+	return GetOuter()->GetWorld();
+}
+
+bool UStatusEffectBase::ActivateStatusEffect(AActor* Instigator, AActor* Target)
+{
+	if (!IsValid(Target))
+	{
+		MarkAsGarbage();
+		return false;
+	}
+	
+	OwningManager = Target->GetComponentByClass<UStatusEffectsManagerComponent>();
+
+	if (!IsValid(OwningManager))
+	{
+		MarkAsGarbage();
+		return false;
+	}
+	
+	TargetActor = Target;
+	InstigatorActor = Instigator;
+	const bool bResult = ActivationEvent();
+
+	if (!bResult)
+	{
+		MarkAsGarbage();
+	}
+
+	return bResult;
+}
+
+bool UStatusEffectBase::DeactivateStatusEffect(AActor* Deactivator)
+{
+	if (!IsValid(this))
+	{
+		return false;
+	}
+
+	const bool bResult = DeactivationEvent(Deactivator);
+
+	if (bResult)
+	{
+		MarkAsGarbage();
+	}
+	
+	return bResult;
+}
+
+bool UStatusEffectBase::ActivationEvent_Implementation()
+{
+	return false;
+}
+
+void UStatusEffectBase::TickEvent_Implementation(float DeltaTime)
+{
+}
+
+bool UStatusEffectBase::DeactivationEvent_Implementation(AActor* Deactivator)
+{
+	return false;
+}
+
+
+TStatId UStatusEffectBase::GetStatId() const
+{
+	RETURN_QUICK_DECLARE_CYCLE_STAT(UStatusEffectBase, STATGROUP_Tickables);
+}
---
+++ Source/TrickyStatusEffects/Public/StatusEffectBase.h
@@ -0,0 +1,67 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "StatusEffectBase.generated.h"
+
+class UStatusEffectsManagerComponent;
+/**
+ * 
+ */
+UCLASS()
+class TRICKYSTATUSEFFECTS_API UStatusEffectBase : public UObject, public FTickableGameObject
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool IsTickable() const override;
+
+	virtual bool IsTickableWhenPaused() const override;
+
+	virtual UWorld* GetTickableGameObjectWorld() const override;
+
+	virtual bool ActivateStatusEffect(AActor* Instigator, AActor* Target);
+
+	virtual bool DeactivateStatusEffect(AActor* Deactivator);
+
+	UFUNCTION(BlueprintGetter, Category = "StatusEffects")
+	AActor* GetTargetActor() const { return TargetActor; }
+
+	UFUNCTION(BlueprintGetter, Category = "StatusEffects")
+	AActor* GetInstigatorActor() const { return InstigatorActor; }
+
+	UFUNCTION(BlueprintGetter, Category = "StatusEffects")
+	UStatusEffectsManagerComponent* GetOwningManager() const { return OwningManager; }
+
+protected:
+	UFUNCTION(BlueprintNativeEvent, Category="StatusEffect")
+	bool ActivationEvent();
+
+	virtual bool ActivationEvent_Implementation();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "StatusEffect")
+	void TickEvent(float DeltaTime);
+
+	virtual void TickEvent_Implementation(float DeltaTime);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "StatusEffect")
+	bool DeactivationEvent(AActor* Deactivator);
+
+	virtual bool DeactivationEvent_Implementation(AActor* Deactivator);
+
+private:
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual TStatId GetStatId() const override;
+
+	UPROPERTY(BlueprintGetter=GetTargetActor, Category="StatusEffect")
+	AActor* TargetActor = nullptr;
+
+	UPROPERTY(BlueprintGetter=GetInstigatorActor, Category="StatusEffect")
+	AActor* InstigatorActor = nullptr;
+
+	UPROPERTY(BlueprintGetter=GetOwningManager, Category="StatusEffect")
+	TObjectPtr<UStatusEffectsManagerComponent> OwningManager = nullptr;
+};
