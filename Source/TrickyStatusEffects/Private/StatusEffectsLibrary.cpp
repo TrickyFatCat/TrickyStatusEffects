ce5818ce1577fb5a3e2730f97fe6d44575c8ea7e
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-05 23:10:08 +0300

    Create StatusEffectsLibrary blueprint function library

---
+++ Source/TrickyStatusEffects/Private/StatusEffectsLibrary.cpp
@@ -0,0 +1,548 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "StatusEffectsLibrary.h"
+
+#include "StatusEffectsManagerComponent.h"
+
+UStatusEffectsManagerComponent* UStatusEffectsLibrary::GetStatusEffectsManager(AActor* Target)
+{
+	if (!IsValid(Target))
+	{
+		return nullptr;
+	}
+
+	return Target->FindComponentByClass<UStatusEffectsManagerComponent>();
+}
+
+UStatusEffectBase* UStatusEffectsLibrary::ApplyStatusEffect(AActor* Target,
+                                                            const TSubclassOf<UStatusEffectBase> StatusEffect,
+                                                            AActor* Instigator)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return nullptr;
+	}
+
+	UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return nullptr;
+	}
+
+	return StatusEffectsManager->ApplyStatusEffect(StatusEffect, Instigator);
+}
+
+void UStatusEffectsLibrary::RefreshAllStatusEffects(AActor* Target)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RefreshAllStatusEffects();
+}
+
+void UStatusEffectsLibrary::RefreshAllStatusEffectsOfClass(AActor* Target, TSubclassOf<UStatusEffectBase> StatusEffect)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RefreshAllStatusEffectsOfClass(StatusEffect);
+}
+
+void UStatusEffectsLibrary::RefreshAllStatusEffectsFromInstigator(AActor* Target, AActor* Instigator)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RefreshAllStatusEffectsFromInstigator(Instigator);
+}
+
+void UStatusEffectsLibrary::RefreshAllStatusEffectsOfClassFromInstigator(AActor* Target,
+                                                                         TSubclassOf<UStatusEffectBase> StatusEffect,
+                                                                         AActor* Instigator)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RefreshAllStatusEffectsOfClassFromInstigator(StatusEffect, Instigator);
+}
+
+void UStatusEffectsLibrary::RefreshAllStatusEffectsOfType(AActor* Target, const EStatusEffectType StatusEffectType)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RefreshAllStatusEffectsOfType(StatusEffectType);
+}
+
+void UStatusEffectsLibrary::RefreshAllStatusEffectsOfTypeFromInstigator(AActor* Target,
+                                                                        const EStatusEffectType StatusEffectType,
+                                                                        AActor* Instigator)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RefreshAllStatusEffectsOfTypeFromInstigator(StatusEffectType, Instigator);
+}
+
+bool UStatusEffectsLibrary::RemoveStatusEffect(AActor* Target,
+                                               const TSubclassOf<UStatusEffectBase> StatusEffect,
+                                               AActor* Remover)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return false;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return false;
+	}
+
+	return StatusEffectsManager->RemoveStatusEffect(StatusEffect, Remover);
+}
+
+bool UStatusEffectsLibrary::RemoveStatusEffectFromInstigator(AActor* Target,
+                                                             const TSubclassOf<UStatusEffectBase> StatusEffect,
+                                                             AActor* Instigator,
+                                                             AActor* Remover)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return false;
+	}
+
+	UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return false;
+	}
+
+	return StatusEffectsManager->RemoveStatusEffectFromInstigator(StatusEffect, Instigator, Remover);
+}
+
+void UStatusEffectsLibrary::RemoveAllStatusEffects(AActor* Target, AActor* Remover)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RemoveAllStatusEffects(Remover);
+}
+
+void UStatusEffectsLibrary::RemoveAllStatusEffectsFromInstigator(AActor* Target, AActor* Instigator, AActor* Remover)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RemoveAllStatusEffectsFromInstigator(Remover, Instigator);
+}
+
+void UStatusEffectsLibrary::RemoveAllStatusEffectsOfClass(AActor* Target,
+                                                          const TSubclassOf<UStatusEffectBase> StatusEffect,
+                                                          AActor* Remover)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RemoveAllStatusEffectsOfClass(StatusEffect, Remover);
+}
+
+void UStatusEffectsLibrary::RemoveAllStatusEffectsOfClassFromInstigator(AActor* Target,
+                                                                        TSubclassOf<UStatusEffectBase> StatusEffect,
+                                                                        AActor* Instigator,
+                                                                        AActor* Remover)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RemoveAllStatusEffectsOfClassFromInstigator(StatusEffect, Instigator, Remover);
+}
+
+void UStatusEffectsLibrary::RemoveAllStatusEffectsOfType(AActor* Target,
+                                                         const EStatusEffectType StatusEffectType,
+                                                         AActor* Remover)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RemoveAllStatusEffectsOfType(StatusEffectType, Remover);
+}
+
+void UStatusEffectsLibrary::RemoveAllStatusEffectsOfTypeFromInstigator(AActor* Target,
+                                                                       EStatusEffectType StatusEffectType,
+                                                                       AActor* Instigator,
+                                                                       AActor* Remover)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->RemoveAllStatusEffectsOfTypeFromInstigator(StatusEffectType, Instigator, Remover);
+}
+
+bool UStatusEffectsLibrary::HasStatusEffect(AActor* Target, TSubclassOf<UStatusEffectBase> StatusEffect)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return false;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return false;
+	}
+
+	return StatusEffectsManager->HasStatusEffect(StatusEffect);
+}
+
+bool UStatusEffectsLibrary::HasStatusEffectFromInstigator(AActor* Target,
+                                                          TSubclassOf<UStatusEffectBase> StatusEffect,
+                                                          AActor* Instigator)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return false;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return false;
+	}
+
+	return StatusEffectsManager->HasStatusEffectFromInstigator(StatusEffect, Instigator);
+}
+
+bool UStatusEffectsLibrary::HasAnyStatusEffect(AActor* Target)
+{
+	if (!IsValid(Target))
+	{
+		return false;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return false;
+	}
+
+	return StatusEffectsManager->HasAnyStatusEffect();
+}
+
+bool UStatusEffectsLibrary::HasAnyStatusEffectFromInstigator(AActor* Target, AActor* Instigator)
+{
+	if (!IsValid(Target))
+	{
+		return false;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return false;
+	}
+
+	return StatusEffectsManager->HasAnyStatusEffectFromInstigator(Instigator);
+}
+
+bool UStatusEffectsLibrary::HasAnyStatusEffectOfType(AActor* Target, const EStatusEffectType StatusEffectType)
+{
+	if (!IsValid(Target))
+	{
+		return false;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return false;
+	}
+
+	return StatusEffectsManager->HasAnyStatusEffectOfType(StatusEffectType);
+}
+
+bool UStatusEffectsLibrary::HasAnyStatusEffectOfTypeFromInstigator(AActor* Target,
+                                                                   const EStatusEffectType StatusEffectType,
+                                                                   AActor* Instigator)
+{
+	if (!IsValid(Target))
+	{
+		return false;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return false;
+	}
+
+	return StatusEffectsManager->HasAnyStatusEffectOfTypeFromInstigator(StatusEffectType, Instigator);
+}
+
+UStatusEffectBase* UStatusEffectsLibrary::GetStatusEffect(AActor* Target, TSubclassOf<UStatusEffectBase> StatusEffect)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return nullptr;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return nullptr;
+	}
+
+	return StatusEffectsManager->GetStatusEffect(StatusEffect);
+}
+
+UStatusEffectBase* UStatusEffectsLibrary::GetStatusEffectFromInstigator(AActor* Target,
+                                                                        TSubclassOf<UStatusEffectBase> StatusEffect,
+                                                                        AActor* Instigator)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return nullptr;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return nullptr;
+	}
+
+	return StatusEffectsManager->GetStatusEffectFromInstigator(StatusEffect, Instigator);
+}
+
+void UStatusEffectsLibrary::GetAllStatusEffects(AActor* Target, TArray<UStatusEffectBase*>& OutStatusEffects)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->GetAllStatusEffects(OutStatusEffects);
+}
+
+void UStatusEffectsLibrary::GetAllStatusEffectsOfClass(AActor* Target,
+                                                       TArray<UStatusEffectBase*>& OutStatusEffects,
+                                                       TSubclassOf<UStatusEffectBase> StatusEffect)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return;
+	}
+
+	UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->GetAllStatusEffectsOfClass(OutStatusEffects, StatusEffect);
+}
+
+void UStatusEffectsLibrary::GetAllStatusEffectsFromInstigator(AActor* Target,
+                                                              TArray<UStatusEffectBase*>& OutStatusEffects,
+                                                              AActor* Instigator)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->GetAllStatusEffectsFromInstigator(OutStatusEffects, Instigator);
+}
+
+void UStatusEffectsLibrary::GetAllStatusEffectsOfClassFromInstigator(AActor* Target,
+                                                                     TArray<UStatusEffectBase*>& OutStatusEffects,
+                                                                     TSubclassOf<UStatusEffectBase> StatusEffect,
+                                                                     AActor* Instigator)
+{
+	if (!IsValid(Target) || !IsValid(StatusEffect))
+	{
+		return;
+	}
+
+	UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->GetAllStatusEffectsOfClassFromInstigator(OutStatusEffects, StatusEffect, Instigator);
+}
+
+void UStatusEffectsLibrary::GetAllStatusEffectsOfType(AActor* Target,
+                                                      TArray<UStatusEffectBase*>& OutStatusEffects,
+                                                      const EStatusEffectType StatusEffectType)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->GetAllStatusEffectsOfType(OutStatusEffects, StatusEffectType);
+}
+
+void UStatusEffectsLibrary::GetAllStatusEffectsOfTypeFromInstigator(AActor* Target,
+                                                                    TArray<UStatusEffectBase*>& OutStatusEffects,
+                                                                    const EStatusEffectType StatusEffectType,
+                                                                    AActor* Instigator)
+{
+	if (!IsValid(Target))
+	{
+		return;
+	}
+
+	const UStatusEffectsManagerComponent* StatusEffectsManager = GetStatusEffectsManager(Target);
+
+	if (!IsValid(StatusEffectsManager))
+	{
+		return;
+	}
+
+	StatusEffectsManager->GetAllStatusEffectsOfTypeFromInstigator(OutStatusEffects, StatusEffectType, Instigator);
+}
---
+++ Source/TrickyStatusEffects/Public/StatusEffectsLibrary.h
@@ -0,0 +1,146 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "StatusEffectsLibrary.generated.h"
+
+class UStatusEffectBase;
+enum class EStatusEffectType : uint8;
+class UStatusEffectsManagerComponent;
+/**
+ * 
+ */
+UCLASS()
+class TRICKYSTATUSEFFECTS_API UStatusEffectsLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static UStatusEffectsManagerComponent* GetStatusEffectsManager(AActor* Target);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static UStatusEffectBase* ApplyStatusEffect(AActor* Target,
+	                                            TSubclassOf<UStatusEffectBase> StatusEffect,
+	                                            AActor* Instigator);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RefreshAllStatusEffects(AActor* Target);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RefreshAllStatusEffectsOfClass(AActor* Target, TSubclassOf<UStatusEffectBase> StatusEffect);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RefreshAllStatusEffectsFromInstigator(AActor* Target, AActor* Instigator);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RefreshAllStatusEffectsOfClassFromInstigator(AActor* Target,
+	                                                         TSubclassOf<UStatusEffectBase> StatusEffect,
+	                                                         AActor* Instigator);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RefreshAllStatusEffectsOfType(AActor* Target, const EStatusEffectType StatusEffectType);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RefreshAllStatusEffectsOfTypeFromInstigator(AActor* Target,
+	                                                        const EStatusEffectType StatusEffectType,
+	                                                        AActor* Instigator);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static bool RemoveStatusEffect(AActor* Target, TSubclassOf<UStatusEffectBase> StatusEffect, AActor* Remover);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static bool RemoveStatusEffectFromInstigator(AActor* Target,
+	                                             TSubclassOf<UStatusEffectBase> StatusEffect,
+	                                             AActor* Instigator,
+	                                             AActor* Remover);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RemoveAllStatusEffects(AActor* Target, AActor* Remover);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RemoveAllStatusEffectsFromInstigator(AActor* Target, AActor* Instigator, AActor* Remover);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RemoveAllStatusEffectsOfClass(AActor* Target,
+	                                          const TSubclassOf<UStatusEffectBase> StatusEffect,
+	                                          AActor* Remover);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RemoveAllStatusEffectsOfClassFromInstigator(AActor* Target,
+	                                                        TSubclassOf<UStatusEffectBase> StatusEffect,
+	                                                        AActor* Instigator,
+	                                                        AActor* Remover);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RemoveAllStatusEffectsOfType(AActor* Target, EStatusEffectType StatusEffectType, AActor* Remover);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void RemoveAllStatusEffectsOfTypeFromInstigator(AActor* Target,
+	                                                       EStatusEffectType StatusEffectType,
+	                                                       AActor* Instigator,
+	                                                       AActor* Remover);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static bool HasStatusEffect(AActor* Target, TSubclassOf<UStatusEffectBase> StatusEffect);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static bool HasStatusEffectFromInstigator(AActor* Target,
+	                                          TSubclassOf<UStatusEffectBase> StatusEffect,
+	                                          AActor* Instigator);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static bool HasAnyStatusEffect(AActor* Target);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static bool HasAnyStatusEffectFromInstigator(AActor* Target, AActor* Instigator);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static bool HasAnyStatusEffectOfType(AActor* Target, const EStatusEffectType StatusEffectType);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static bool HasAnyStatusEffectOfTypeFromInstigator(AActor* Target,
+	                                                   const EStatusEffectType StatusEffectType,
+	                                                   AActor* Instigator);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static UStatusEffectBase* GetStatusEffect(AActor* Target, TSubclassOf<UStatusEffectBase> StatusEffect);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static UStatusEffectBase* GetStatusEffectFromInstigator(AActor* Target,
+	                                                        TSubclassOf<UStatusEffectBase> StatusEffect,
+	                                                        AActor* Instigator);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void GetAllStatusEffects(AActor* Target, TArray<UStatusEffectBase*>& OutStatusEffects);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void GetAllStatusEffectsOfClass(AActor* Target,
+	                                       TArray<UStatusEffectBase*>& OutStatusEffects,
+	                                       TSubclassOf<UStatusEffectBase> StatusEffect);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void GetAllStatusEffectsFromInstigator(AActor* Target,
+	                                              TArray<UStatusEffectBase*>& OutStatusEffects,
+	                                              AActor* Instigator);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void GetAllStatusEffectsOfClassFromInstigator(AActor* Target,
+	                                                     TArray<UStatusEffectBase*>& OutStatusEffects,
+	                                                     TSubclassOf<UStatusEffectBase> StatusEffect,
+	                                                     AActor* Instigator);
+
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void GetAllStatusEffectsOfType(AActor* Target,
+	                                      TArray<UStatusEffectBase*>& OutStatusEffects,
+	                                      EStatusEffectType StatusEffectType);
+
+	UFUNCTION(BlueprintPure, Category="StatusEffects", meta=(WorldContext="Target"))
+	static void GetAllStatusEffectsOfTypeFromInstigator(AActor* Target,
+	                                                    TArray<UStatusEffectBase*>& OutStatusEffects,
+	                                                    EStatusEffectType StatusEffectType,
+	                                                    AActor* Instigator);
+};
