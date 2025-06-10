445e05c2f5d834b96d3ae6c03dbcc76158aaa6d1
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-10 22:01:53 +0300

    Remove unused dependencies `Slate` and `SlateCore` in build configuration

--- Source/TrickyStatusEffects/TrickyStatusEffects.Build.cs
+++ Source/TrickyStatusEffects/TrickyStatusEffects.Build.cs
@@ -36,8 +36,6 @@ public class TrickyStatusEffects : ModuleRules
 			{
 				"CoreUObject",
 				"Engine",
-				"Slate",
-				"SlateCore",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
